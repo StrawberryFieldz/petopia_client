c5561ff724a3678076b5733782ea14641f957212
Jeff Van Dalsum <jeffvan576@gmail.com>
2014-09-19 16:02:52 -0700

    adds serve and lint tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
+
+var paths = {
+	scripts: ['scripts/**/*.js']
+}
 
 gulp.task('default', function() {
 
@@ -7,4 +13,17 @@ gulp.task('default', function() {
 
 gulp.task('karma', shell.task([
 	'karma start'
-]))
+]));
+
+gulp.task('lint', function(){
+	return gulp.src(paths.scripts)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('serve', function(){
+	nodemon({ script: 'server.js', ignore: 'node_modules/**/*.js' })
+	  .on('restart', function(){
+	  	console.log('Server restarted!!!');
+	  })
+});
