64fdb804d3b05bc637ab1f97288bf021b9e59d9f
Tyler <tyhlewis@gmail.com>
2014-09-20 15:28:10 -0700

    (feat) adds basic structure to navbar

--- scripts/components/navbar/navbar.controller.js
+++ scripts/components/navbar/navbar.controller.js
@@ -1,4 +1,11 @@
 angular.module('app')
   .controller('NavbarController', function($scope, $state){
+    // User login status
+    $scope.loggedIn = false;
+
+    // Directs user to home page
+    $scope.goHome = function() {
+      $state.go('splash');
+    };
 
   });
