22e79f3a9e59372bdceb4a1f5389651aac1280c1
Jeff Van Dalsum <jeffvan576@gmail.com>
2014-09-19 17:15:38 -0700

    (feat) Create Gulp files to enable tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,23 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
+var karma = require('gulp-karma');
 
 var paths = {
-	scripts: ['scripts/**/*.js']
+	scripts: ['scripts/**/*.js'],
+	karmaTestFiles: 'scripts/**/*.spec.js',
+	karmaConfigFile: 'karma.conf.js'
 }
 
 gulp.task('default', function() {
 
 });
 
-gulp.task('test', shell.task([
-	'karma start'
-]));
+gulp.task('test', function(){
+	return gulp
+	  .src(paths.karmaTestFiles)
+	  .pipe(karma({configFile: paths.karmaConfigFile, action: 'run'}));
+});
 
 gulp.task('lint', function(){
 	return gulp.src(paths.scripts)
