d65898bbb2aea879c3b3c9125310e065761eff1c
Tyler <tyhlewis@gmail.com>
2014-09-20 16:25:40 -0700

    (test) adds unit testing to navbar

--- scripts/components/navbar/navbar.spec.js
+++ scripts/components/navbar/navbar.spec.js
@@ -1,9 +1,41 @@
-describe('navbar', function () {
+describe('NavbarController', function () {
   'use strict';
 
-  it('exists', function () {
-    expect(true).to.be(false);
+  beforeEach(module('app'));
 
+  var ctrl, scope;
+  // inject the $controller and $rootScope services
+  // in the beforeEach block
+  beforeEach(inject(function($controller, $rootScope) {
+    // Create a new scope that's a child of the $rootScope
+    scope = $rootScope.$new();
+    // Create the controller
+    ctrl = $controller('NavbarController', {
+      $scope: scope
+    });
+
+  }));
+
+  it('should exist', function() {
+    expect(ctrl).to.not.equal(undefined);
+  });
+
+  it('should check to see if user is logged in', function() {
+    expect(scope.loggedIn).to.not.equal(undefined);
+  });
+
+  it('should direct users to splash page when logo is clicked', function() {
+    expect(scope.goHome).to.be.a('function');
+  });
+
+  it('should display popup when "Sign Up" or "Log In" buttons are clicked', function() {
+    expect(scope.callPopup).to.be.a('function');
+  });
+
+  it('should display *username* and "Sign Out" buttons instead of "Sign Up" and "Log In" buttons if user is logged in', function() {
+    scope.loggedIn = false;
+    scope.toggleLoginStatus();
+    expect(scope.loggedIn).to.equal(true);
   });
 
 });
