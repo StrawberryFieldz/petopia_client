cd3da3d5be1d42b99c0c496a95e3240c334d4812
Tyler <tyhlewis@gmail.com>
2014-09-20 16:25:10 -0700

    (feat) adds basic functionality to navbar

--- scripts/components/navbar/navbar.controller.js
+++ scripts/components/navbar/navbar.controller.js
@@ -1,11 +1,23 @@
 angular.module('app')
-  .controller('NavbarController', function($scope, $state){
+  .controller('NavbarController', function($scope, $state, Navbar){
     // User login status
-    $scope.loggedIn = false;
+    $scope.loggedIn = Navbar.loggedIn;
+
+    // Toggle login status if user is logged in or if user logs out
+    $scope.toggleLoginStatus = function() {
+      Navbar.loggedIn = !Navbar.loggedIn;
+      $scope.loggedIn = Navbar.loggedIn;
+    };
 
     // Directs user to home page
     $scope.goHome = function() {
       $state.go('splash');
+    };
+
+    $scope.callPopup = function(context) {
+      // Will need to include Popup factory and pass context to Popup function that handles
+      // sign up vs. log in views
+      $state.go('popup');
     };
 
   });
