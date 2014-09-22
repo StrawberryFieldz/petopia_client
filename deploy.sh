9fffb72fd325782bda2d43af974909db17f08384
Jeff Van Dalsum <jeffvan576@gmail.com>
2014-09-22 16:05:50 -0700

    adds build task to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -121,7 +121,7 @@ fi
 if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then  
   eval $NPM_CMD install --global gulp  
   exitWithMessageOnError "installing gulp failed"  
-  ./node_modules/.bin/gulp 
+  ./node_modules/.bin/gulp build
   exitWithMessageOnError "gulp failed"  
 fi  
   
