52d369cb5ad36779160047e300412d46f2d7c744
Jeff Van Dalsum <jeffvan576@gmail.com>
2014-09-22 12:45:11 -0700

    added openbrowser, concat, and build tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,40 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var opn = require('opn');
 var karma = require('karma').server;
+var minifyCSS = require('gulp-minify-css');
 
 var paths = {
 	scripts: ['scripts/**/*.js'],
 	karmaTestFiles: 'scripts/**/*.spec.js',
-	karmaConfigFile: 'karma.conf.js'
+	karmaConfigFile: 'karma.conf.js',
+	compressFiles: '<what files to uglify>',
+	concatFiles: ['scripts/app.js', 'scripts/**/*.controller.js', 'scripts/**/*.services.js']
 }
 
 gulp.task('default', function() {
 
+});
+
+// rename
+gulp.task('compress', function() {
+  gulp.src(paths.compressFiles)
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+});
+
+// rename
+gulp.task('concat', function() {
+  gulp.src(paths.concatFiles)
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('openbrowser', function() {
+  opn( 'http://localhost:8000/' );
 });
 
 gulp.task('test', function (done) {
@@ -37,3 +61,5 @@ gulp.task('serve', function(){
 gulp.task('watch', function(){
 	gulp.watch(paths.scripts, ['lint'])
 });
+
+gulp.task('build', ['concat', 'serve', 'openbrowser']);
