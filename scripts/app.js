ea7b60519ab8c82b1059c28e462da4e0b0a00078
Dominic Czarnota <dczarno@gmail.com>
2014-09-22 22:18:33 -0700

    (feat) adds ngMaterial to dependencies

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ui.router'])
+angular.module('app', ['ui.router', 'ngMaterial'])
 
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   // Routes to home page on default
