96a337ddc06b13fe117ebc2033b2163a2630de96
Dominic Czarnota <dczarno@gmail.com>
2014-09-22 17:14:51 -0700

    (feat) changes default browser to PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // testBrowser will be Chrome unless running TravisCI (then it will be PhantomJS)
-    browsers: [testBrowser],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
