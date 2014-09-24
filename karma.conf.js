d3b152071e8ae5122b279a22781bf0409ce37d5a
Dominic Czarnota <dczarno@gmail.com>
2014-09-24 14:59:39 -0700

    (fix) adds angular material and angular animate to files list

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,8 @@ module.exports = function(config) {
         'bower_components/angular/angular.js',
         'bower_components/angular-mocks/angular-mocks.js',
         'bower_components/angular-ui-router/release/angular-ui-router.js',
+        'bower_components/angular-material/angular-material.js',
+        'bower_components/angular-animate/angular-animate.js',
         'scripts/**/*.js',
         'scripts/**/*.spec.js',
         'scripts/**/*.html',
