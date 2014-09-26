0fdc8273d826b1e65cec1a426e2e59625ca16d30
Wil A <andradewilker@gmail.com>
2014-09-25 20:29:26 -0700

    Added more items to stub data to help visualize app.

--- scripts/search/search.services.js
+++ scripts/search/search.services.js
@@ -50,6 +50,100 @@ angular.module('app')
 				cost: 65,
 				rating: 3
 			},
+			{
+				name: "Dominic",
+				location: "LA",
+				zip: 84732,
+				photo: "https://avatars2.githubusercontent.com/u/6237873?v=2&s=400",
+				cost: 60,
+				rating: 4
+			},
+			{
+				name: "Jeff",
+				location: "Boston",
+				zip: 03062,
+				photo: "https://avatars2.githubusercontent.com/u/7004952?v=2&s=400",
+				cost: 40,
+				rating: 1
+			},
+			{
+				name: "Bob",
+				location: "San Francisco",
+				zip: 12345,
+				photo: "http://www.mateoj.com/bayside/wp-content/uploads/2014/07/521013543_1385596410.jpg",
+				cost: 15,
+				rating: 2
+			},
+			{
+				name: "Sam",
+				location: "San Francisco",
+				zip: 94103,
+				photo: "http://static.ddmcdn.com/gif/dog-breed-dog-breed-pictures1.jpg",
+				cost: 65,
+				rating: 3
+			},{
+				name: "Dominic",
+				location: "LA",
+				zip: 84732,
+				photo: "https://avatars2.githubusercontent.com/u/6237873?v=2&s=400",
+				cost: 60,
+				rating: 4
+			},
+			{
+				name: "Jeff",
+				location: "Boston",
+				zip: 03062,
+				photo: "https://avatars2.githubusercontent.com/u/7004952?v=2&s=400",
+				cost: 40,
+				rating: 1
+			},
+			{
+				name: "Bob",
+				location: "San Francisco",
+				zip: 12345,
+				photo: "http://www.mateoj.com/bayside/wp-content/uploads/2014/07/521013543_1385596410.jpg",
+				cost: 15,
+				rating: 2
+			},
+			{
+				name: "Sam",
+				location: "San Francisco",
+				zip: 94103,
+				photo: "http://static.ddmcdn.com/gif/dog-breed-dog-breed-pictures1.jpg",
+				cost: 65,
+				rating: 3
+			},{
+				name: "Dominic",
+				location: "LA",
+				zip: 84732,
+				photo: "https://avatars2.githubusercontent.com/u/6237873?v=2&s=400",
+				cost: 60,
+				rating: 4
+			},
+			{
+				name: "Jeff",
+				location: "Boston",
+				zip: 03062,
+				photo: "https://avatars2.githubusercontent.com/u/7004952?v=2&s=400",
+				cost: 40,
+				rating: 1
+			},
+			{
+				name: "Bob",
+				location: "San Francisco",
+				zip: 12345,
+				photo: "http://www.mateoj.com/bayside/wp-content/uploads/2014/07/521013543_1385596410.jpg",
+				cost: 15,
+				rating: 2
+			},
+			{
+				name: "Sam",
+				location: "San Francisco",
+				zip: 94103,
+				photo: "http://static.ddmcdn.com/gif/dog-breed-dog-breed-pictures1.jpg",
+				cost: 65,
+				rating: 3
+			},
 		];
   	return {
   		GetSitters: function(callback){
