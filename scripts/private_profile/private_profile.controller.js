e338886ff61b6a9387f1f3fd8f4bc7b230405acb
Jeff Van Dalsum <jeffvan576@gmail.com>
2014-09-29 18:09:05 -0700

    adds prefill ability to private profile become a sitter form

--- scripts/private_profile/private_profile.controller.js
+++ scripts/private_profile/private_profile.controller.js
@@ -1,10 +1,12 @@
 angular.module('app')
   .controller('PrivateProfileController', ['$scope', '$state', 'SitterManager', 'PrivateProfileService', function($scope, $state, SitterManager, PrivateProfileService){
+    
+    // change this to call the database and pull in any information for the user
     $scope.petSitter = {
-      name: '',
-      userLocation: '',
+      name: 'Jeff',
+      userLocation: 'San Francisco',
       bio: '',
-      dailyRate: '',
+      dailyRate: '$10',
       petToWatchObject: {
         dog: false,
         cat: false
