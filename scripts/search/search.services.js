1164dd489f685793b1f850bd02b2166d582deca7
Jeff Van Dalsum <jeffvan576@gmail.com>
2014-10-02 19:06:59 -0700

    updates stub data information

--- scripts/search/search.services.js
+++ scripts/search/search.services.js
@@ -19,7 +19,7 @@ angular.module('app')
 				zip: 94103,
 				photo: "https://avatars2.githubusercontent.com/u/7147319?v=2&s=400",
 				cost: 99,
-				rating: 5,
+				rating: 1,
 				bio: "Lorem ipsum dolor sit amet, ne quod novum mei.Sea omnium invenire mediocrem at, in lobortis conclusionemque nam. Ne deleniti appetere reprimique pro, inani labitur disputationi te sed. At vix sale omnesque, id pro labitur reformidans accommodare, cum labores honestatis eu. Nec quem lucilius in, eam praesent reformidans no. Sed laudem aliquam ne.",
 		        dogs:true,
 		        cats: false
@@ -41,7 +41,7 @@ angular.module('app')
 				zip: 03062,
 				photo: "https://avatars2.githubusercontent.com/u/7004952?v=2&s=400",
 				cost: 40,
-				rating: 1,
+				rating: 2,
 				bio: "Lorem ipsum dolor sit amet, ne quod novum mei.Sea omnium invenire mediocrem at, in lobortis conclusionemque nam. Ne deleniti appetere reprimique pro, inani labitur disputationi te sed. At vix sale omnesque, id pro labitur reformidans accommodare, cum labores honestatis eu. Nec quem lucilius in, eam praesent reformidans no. Sed laudem aliquam ne.",
 		        dogs:false,
 		        cats: false
