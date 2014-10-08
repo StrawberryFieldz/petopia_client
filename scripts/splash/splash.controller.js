1e9a2ad1f8970be6ae39bf8334ed5a245d5b6d22
Dominic Czarnota <dczarno@gmail.com>
2014-10-07 22:28:07 -0700

    (feat) adds function to go to bottom of page on user click

--- scripts/splash/splash.controller.js
+++ scripts/splash/splash.controller.js
@@ -1,11 +1,21 @@
 angular.module('app')
-  .controller('SplashController', ['$scope', '$state', 'CityFactory', function($scope, $state, CityFactory) {
+  .controller('SplashController', ['$scope', '$state', '$location', '$anchorScroll', 'CityFactory', function($scope, $state, $location, $anchorScroll, CityFactory) {
     $scope.setCity = function(cityName){
       CityFactory.setCity(cityName);
       if(CityFactory.getCity()){
         $state.go('search');
       }
 
+    };
+
+    // jump to bottom of page when user clicks on white down arrow
+    $scope.goToBottom = function(){
+      //set the location.hash to id="bottom" in the "meetTheTeamSection" material-toolbar
+      $location.hash('bottom');
+      //call $anchorScroll() to jump to bottom of page
+      $anchorScroll();
+      //clear url path so that it does not display the id in the url path
+      $location.url($location.path());
     };
 
   }]);
