17c63e52a32e910c1b68d031b41197b4940483bc
Wil A <andradewilker@gmail.com>
2014-10-07 21:01:35 -0700

    Client will now use api for sitter info.

--- scripts/search/search.controller.js
+++ scripts/search/search.controller.js
@@ -1,13 +1,19 @@
 angular.module('app')
   .controller('SearchController', ['$scope', '$state', 'CityFactory', 'SitterManager', 'PawIconManager', function($scope, $state, CityFactory, SitterManager, PawIconManager){
 
-    $scope.sitters = SitterManager.GetStubData();
+    var city = CityFactory.getCity();
+    console.log("city is currently: ",city);
+
+    SitterManager.GetSitters(function(data){
+      console.log(data);
+      $scope.sitters = data;
+    });
     
-    $scope.setCity = function(cityName){
-      console.log(cityName);
-      CityFactory.setCity(cityName);
-      $scope.city = CityFactory.getCity();
-    };
+    // $scope.setCity = function(cityName){
+    //   console.log(cityName);
+    //   CityFactory.setCity(cityName);
+    //   // $scope.city = CityFactory.getCity();
+    // };
 
     $scope.getRating = function(sitter){
       return PawIconManager.GetIcons(sitter.rating);
